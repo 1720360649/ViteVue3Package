2ed2364aea97239c3b329bda3c53a6c0a00c5d51
WN <56852884+1720360649@users.noreply.github.com>
2023-11-18 02:54:21 +0800

    增加css打包和自动导入

--- packages/wnview/button/button.vue
+++ packages/wnview/button/button.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <button>测试</button>
+    <button>
+      <span class="text">测试</span>
+    </button>
   </div>
 </template>
+<script lang="ts" setup>
+
+</script>
+<style lang="scss">
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  .text{
+    color: green;
+  }
+</style>
--- packages/wnview/vite.config.ts
+++ packages/wnview/vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue"
-import dts from 'vite-plugin-dts'
+import vue from "@vitejs/plugin-vue";
+import dts from 'vite-plugin-dts';
+
 export default defineConfig(
   {
     build: {
       target: 'modules',
       //打包文件目录
       outDir: "es",
+      //静态文件目录
+      assetsDir: 'css',
       //压缩
       minify: false,
       //css分离
-      //cssCodeSplit: true,
+      cssCodeSplit: true,
       rollupOptions: {
         //忽略打包vue文件
         external: ['vue'],
@@ -24,6 +27,18 @@ export default defineConfig(
             preserveModules: true,
             //配置打包根目录
             dir: 'dist/es',
+            assetFileNames: (assetInfo) => {
+              //判断是静态资源且是css
+              if (assetInfo.type === 'asset' && /\.(css)$/i.test(assetInfo.name || "")) {
+                //截取出css打包名称中的组件名 button.vue_vue_type_style_index_0_lang.css 截出button
+                let str = (assetInfo.name || 'style.').split('.')[0]
+                //将组件css输出到组件目录下 组件/组件.css
+                return `${str}/${str}.css`
+              } else {
+                //其他静态资源则返回默认的 后续也可以对其他资源文件细分
+                return 'assets/[name].[ext]'
+              }
+            },
             preserveModulesRoot: ''
           },
           {
@@ -33,8 +48,20 @@ export default defineConfig(
             preserveModules: true,
             //配置打包根目录
             dir: 'dist/lib',
+            assetFileNames: (assetInfo) => {
+              //判断是静态资源且是css
+              if (assetInfo.type === 'asset' && /\.(css)$/i.test(assetInfo.name || "")) {
+                //截取出css打包名称中的组件名 button.vue_vue_type_style_index_0_lang.css 截出button
+                let str = (assetInfo.name || 'style.').split('.')[0]
+                //将组件css输出到组件目录下 组件/组件.css
+                return `${str}/${str}.css`
+              } else {
+                //其他静态资源则返回默认的 后续也可以对其他资源文件细分
+                return 'assets/[name].[ext]'
+              }
+            },
             preserveModulesRoot: ''
-          }
+          },
         ]
       },
       lib: {
@@ -51,7 +78,20 @@ export default defineConfig(
       //因为这个插件默认打包到es下，我们想让lib目录下也生成声明文件需要再配置一个
       dts({
         outDir: 'dist/lib',
-      })
+      }),
+      {
+        name: 'vite:import-css',
+        apply: 'build',
+        enforce: 'post',
+        renderChunk(code, chunk) {
+          // 判断是不是组件入口js
+          if(!chunk.isEntry && chunk.type==='chunk' && /\index.(js)$/i.test(chunk.fileName)){
+            // 截取出组件名称
+            let str = chunk.fileName.split('/')[0]
+            return `import './${str}.css';\n${code}`
+          }
+        },
+      }
     ]
   }
 )
